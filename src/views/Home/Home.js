08c3aacf300d36d71ba99e7452649ea2a469163d
Brett Seifried <seifriedbrett@gmail.com>
2021-12-01 11:10:13 -0800

    should have ACP more, completed the blog

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,17 +2,48 @@ import React from 'react';
 
 import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
-
+import { useState } from 'react';
 import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('title');
+  const [subtitle, setSubtitle] = useState('subtitle');
+  const [font, setFont] = useState('font');
+  const [align, setAlign] = useState('align');
+  const [text, setText] = useState('text');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview
+        {...{
+          title,
+          setTitle,
+          subtitle,
+          setSubtitle,
+          font,
+          setFont,
+          align,
+          setAlign,
+          text,
+          setText,
+        }}
+      />
+      <Editor
+        {...{
+          title,
+          setTitle,
+          subtitle,
+          setSubtitle,
+          font,
+          setFont,
+          align,
+          setAlign,
+          text,
+          setText,
+        }}
+      />
     </main>
   );
 }
